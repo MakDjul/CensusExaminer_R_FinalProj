b669bc0ca691d11d35274030870667b891f0f323
Mak Djulbegovic <45323547+MakDjul@users.noreply.github.com>
2019-04-22 14:28:40 -0400

    Create LoadPack.R

---
+++ R Functions/LoadPack.R
@@ -0,0 +1,5 @@
+#Load package depends
+library(CensusExaminer)
+
+#required function to access Census data; generate API key at: http://api.census.gov/data/key_signup.html
+census_api_key("YOUR API KEY GOES HERE")
